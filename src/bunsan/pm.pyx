051c5b9626e813de7ee29fe48db9df7b454d9e16
Aleksey Filippov <sarum9in@gmail.com>
2012-11-06 00:21:31 +0400

    GIL-related fixes.

--- src/bunsan/pm.pyx
+++ src/bunsan/pm.pyx
@@ -21,31 +21,36 @@ cdef str _decode(bytes s):
 
 cdef extern from "bunsan/pm/compatibility/repository.hpp" namespace "bunsan::pm::compatibility":
     cdef cppclass repository:
-        repository(string config) except +
-        void create(string path, bool strip) except +
-        void extract(string package, string path) except +
-        void clean() except +
+        repository(string config) nogil except +
+        void create(string path, bool strip) nogil except +
+        void extract(string package, string path) nogil except +
+        void clean() nogil except +
 
 
 cdef class Repository(object):
 
     cdef repository *thisptr
 
-    def __cinit__(self, config):
+    def __cinit__(self, str config):
         cdef string config_ = _encode(config)
-        self.thisptr = new repository(config_)
+        with nogil:
+            self.thisptr = new repository(config_)
 
     def __dealloc__(self):
-        del self.thisptr
+        with nogil:
+            del self.thisptr
 
-    def create(self, path, strip=False):
+    def create(self, str path, bool strip=False):
         cdef string path_ = _encode(path)
-        self.thisptr.create(path_, strip)
+        with nogil:
+            self.thisptr.create(path_, strip)
 
-    def extract(self, package, path):
+    def extract(self, str package, str path):
         cdef string package_ = _encode(package)
         cdef string path_ = _encode(path)
-        self.thisptr.extract(package_, path_)
+        with nogil:
+            self.thisptr.extract(package_, path_)
 
     def clean(self):
-        self.thisptr.clean()
+        with nogil:
+            self.thisptr.clean()
